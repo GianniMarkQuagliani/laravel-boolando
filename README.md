14da8593c010e34e7c2921fb76500d99655fd05e
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-20 01:12:16 +0100

    add Header

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #
 - Ho creato prima una home scrivendo un semplice h1 per testare
 - Ho creato un main.blade.php, ci ho messo un @yield('content') e @vite('resources/js/app.js') e modificata la home.blade.php, mettendo un semplice Hello World!, così ho creato uno scheletro generale
+- Ho fatto un refactoring
+- Inizio a creare il Menu, faccendo copia e incolla del vecchio progetto e poi includendo nel main
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <div class="topbar">
+        <div class="container row">
+            <nav class="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Prodotti</a></li>
+                    <li><a href="#">Chi Siamo</a></li>
+                    <li><a href="#">Contatti</a></li>
+                </ul>
+            </nav>
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/boolean-logo.png') }}" alt="Logo Boolean">
+            </div>
+            <nav class="menu2">
+                <ul>
+                    <a href="#"><i class="fa-regular fa-user"></i></a>
+                    <a href="#"><i class="fa-regular fa-heart"></i></a>
+                    <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
